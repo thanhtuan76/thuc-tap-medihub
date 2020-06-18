7eeac295ffe4d57a8b13aa7b822e7c4c525bd1bc
thanhtuan76 <58723584+thanhtuan76@users.noreply.github.com>
2020-06-18 12:14:45 +0700

    1806

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,5 +1,22 @@
 $('.multiple-items').slick({
     infinite: true,
-    slidesToShow: 3,
-    slidesToScroll: 3,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    arrows: true,
+    prevArrow: '.prev',
+    nextArrow: '.next',
   });
+
+//   $('.slider-wrap').each(function (index, sliderWrap) {
+//     var $slider = $(sliderWrap).find('.slider');
+//     var $next = $(sliderWrap).find('.next');
+//   var $prev = $(sliderWrap).find('.prev');
+//     $slider.slick({
+//         dots: true,
+//       slidesToShow: 2.5,
+//         nextArrow: $next,
+//       prevArrow: $prev
+//     });
+// });
