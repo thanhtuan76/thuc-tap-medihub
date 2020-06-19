e0dff44a91b90b8215db41f686009ba274e754b0
thanhtuan76 <58723584+thanhtuan76@users.noreply.github.com>
2020-06-19 16:57:33 +0700

    1906

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,13 +1,25 @@
 $('.multiple-items').slick({
-    infinite: true,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    arrows: true,
-    prevArrow: '.prev',
-    nextArrow: '.next',
-  });
+  infinite: true,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  arrows: true,
+  prevArrow: '.prev',
+  nextArrow: '.next',
+});
+
+$('.multiple-items-news').slick({
+  infinite: true,
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  arrows: true,
+  prevArrow: '.prev-news',
+  nextArrow: '.next-news',
+});
+
 
 //   $('.slider-wrap').each(function (index, sliderWrap) {
 //     var $slider = $(sliderWrap).find('.slider');
@@ -20,3 +32,12 @@ $('.multiple-items').slick({
 //       prevArrow: $prev
 //     });
 // });
+
+$('.contact-button-toggle').click(function(e) {
+  e.preventDefault();
+  if($('.contact-button').hasClass('active')) {
+      $('.contact-button').removeClass('active')
+  } else {
+    $('.contact-button').addClass('active')
+  }
+})
